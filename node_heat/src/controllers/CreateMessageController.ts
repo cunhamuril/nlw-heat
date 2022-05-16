47824aa9a0e59b58432699e048f7aeff10f604a4
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-16 15:53:52 -0300

    feat(node_heat): done messages creation endpoint

---
+++ node_heat/src/controllers/CreateMessageController.ts
@@ -0,0 +1,24 @@
+import { Request, response, Response } from "express";
+
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController {
+  async handle(req: Request, res: Response) {
+    const { message } = req.body;
+    const { user_id } = req;
+
+    const service = new CreateMessageService();
+
+    try {
+      const result = await service.execute(message, user_id);
+
+      return res.json(result);
+    } catch (error) {
+      return res
+        .status(error.response.status ?? 500)
+        .json({ error: error.message });
+    }
+  }
+}
+
+export { CreateMessageController };
--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
+
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router };
