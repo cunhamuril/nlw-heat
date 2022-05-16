db049bfbca1691a77c86462ee4a99a05543d365d
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-16 16:53:09 -0300

    feat(node_heat): done user profile endpoint

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -5,6 +5,7 @@ import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3messagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 
 const router = Router();
 
@@ -17,5 +18,7 @@ router.post(
 );
 
 router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
