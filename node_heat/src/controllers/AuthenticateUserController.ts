11483441cbcc634aa404da9bf0e954bfdfff6d3b
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-16 14:47:28 -0300

    feat(node_heat): done user authentication

---
+++ node_heat/src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,23 @@
+import { Request, response, Response } from "express";
+
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+
+class AuthenticateUserController {
+  async handle(req: Request, res: Response) {
+    const { code } = req.body;
+
+    const service = new AuthenticateUserService();
+
+    try {
+      const result = await service.execute(code);
+
+      return res.json(result);
+    } catch (error) {
+      return res
+        .status(error.response.status ?? 500)
+        .json({ error: error.message });
+    }
+  }
+}
+
+export { AuthenticateUserController };
