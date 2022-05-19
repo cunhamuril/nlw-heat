1a4e12a7622dd4fa9f4b768fb8d24fcd6e561673
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-18 22:28:25 -0300

    feat(web): done send message form layout and features

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,3 +1,57 @@
+import { FormEvent, useContext } from "react";
+import { VscGithubInverted, VscSignOut } from "react-icons/vsc";
+import { api } from "../../services/api";
+
+import { AuthContext } from "../../contexts/auth";
+
+import styles from "./styles.module.scss";
+
 export function SendMessageForm() {
-  return <div />;
+  const { user, signOut } = useContext(AuthContext);
+
+  async function handleSendMessage(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const { value: message } = form.message;
+
+    if (!message.trim()) return;
+
+    await api.post("messages", { message });
+
+    form.reset();
+    form.message.focus();
+  }
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button onClick={signOut} className={styles.signOutButton}>
+        <VscSignOut size={32} />
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+
+        <strong className={styles.userName}>{user?.name}</strong>
+
+        <span className={styles.userGithub}>
+          <VscGithubInverted size={16} />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm} onSubmit={handleSendMessage}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual sua expectativa para o evento?"
+        />
+
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
 }
--- web/src/contexts/auth.tsx
+++ web/src/contexts/auth.tsx
@@ -41,6 +41,8 @@ export function AuthProvider({ children }: AuthProvider) {
 
     localStorage.setItem("@dowhile:token", token);
 
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
     setUser(user);
   }
 
